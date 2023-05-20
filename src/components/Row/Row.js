48581e35b25950f598c6d14fcf3ea102364df17e
Ramilatif <latiframi18@gmail.com>
2023-05-20 18:53:20 +0200

    Update Row.js

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -8,16 +8,27 @@ function Row({ title, fetchUrl }) {
     useEffect(() =>{
         async function fetchData(){
         const request = await tmdbApiClient.get(fetchUrl);
-        console.log(request);
+    
+        setMovies(request.data.results);
         return request;
+        
         }
         fetchData()
-        }, []);
-
+        }, [fetchUrl]);
+        
+        console.table(movies);
 
     return(
-        <div>
+        <div className="row">
         <h2>{title}</h2>
+
+        <div className="row_posters">
+        {/*several row_posters */}
+
+        {movies.map(movie =>( <img src={movie.poster_path} alt={movie.name}/>
+        ))}
+
+        </div>
         {/* container -> posters*/}
         
         </div>
