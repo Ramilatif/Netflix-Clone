bbf30c92e5faeafeb7eb631a77b475afd737cae3
Ramilatif <latiframi18@gmail.com>
2023-06-06 10:44:57 +0200

    Banner description is very done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -19,6 +19,10 @@ function Banner() {
   }, []);
   console.log(movie);
 
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) +"..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -31,13 +35,15 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner_title">{movie?.title || movie?.name || movie?.original_name}</h1>
 
         <div className="banner_buttons">
           <button className="bannerbuton">Play</button>
           <button className="bannerbuton">My List</button>
         </div>
-        <h1 className="banner_description">{movie?.overview}</h1>
+        <h1 className="banner_description">{movie?.overview}
+        {truncate(movie?.overview, 150)}
+        </h1>
       </div>
     </header>
   );
