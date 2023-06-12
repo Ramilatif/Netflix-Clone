0a66362f8b5c653ea7d4c149bdd7241693437003
Ramilatif <latiframi18@gmail.com>
2023-06-12 14:16:23 +0200

    NAVBAR finish

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,22 +3,22 @@ import "./Nav.css";
 
 function Nav() {
   const [show, handleShow] = useState(false);
-
-  useEffect(() => {
-    const scrollEventhandler = () => {
+  const scrollEventhandler = () => {
       if (window.scrollY > 100) {
         handleShow(true);
       } else handleShow(false);
     };
 
+  useEffect(() => {
     window.addEventListener("scroll", scrollEventhandler);
+
     return () => {
       window.removeEventListener("scroll", scrollEventhandler);
     };
   }, []);
-
+console.log("show", show)
   return (
-    <div className={`nav ${show && "nav_back"}`}>
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
