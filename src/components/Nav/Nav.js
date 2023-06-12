ea6bb45b624aadab4bba08298fefea377bf55996
Ramilatif <latiframi18@gmail.com>
2023-06-12 10:45:37 +0200

    incomiing

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,36 +1,31 @@
-import React, { useEffect, useState } from "react";
-import "./Nav.css";
+import React, { useEffect } from 'react'
+import "./Nav.css"
 
 function Nav() {
-  const [show, handleShow] = useState(false);
 
-  useEffect(() => {
-    const scrollEventhandler = () => {
-      if (window.scrollY > 100) {
-        handleShow(true);
-      } else handleShow(false);
-    };
-
-    window.addEventListener("scroll", scrollEventhandler);
-    return () => {
-      window.removeEventListener("scroll", scrollEventhandler);
-    };
-  }, []);
+    useEffect(() => {
+        window.addEventListener("scroll", () => { if (window.scrollY> 100){
+            handleShow(true);
+        } else handleShow(false);
+    });
+    return () => {window.removeEventListener("scroll");
+};
+  }, []  )
 
   return (
-    <div className={`nav ${show && "nav_back"}`}>
-      <img
+    <div className="nav">
+        <img 
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt="Netflix Logo"
-      />
-      <img
-        className="nav_avatar"
-        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
-        alt="Netflix Logo"
+        />
+      <img 
+      className="nav_avatar"
+      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+      alt="Netflix Logo"
       />
     </div>
-  );
+  )
 }
 
-export default Nav;
+export default Nav
