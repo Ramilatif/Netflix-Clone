f4533734ce2fec3a37e0d325981b38a5de16efe1
Ramilatif <latiframi18@gmail.com>
2023-06-13 23:36:08 +0200

    past return

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
 import tmdbApiClient from "../../api/TMDBApiClient";
 import "./Row.css";
-import movieTrailer from "movie-trailer";
-import YouTube from "react-youtube"
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
-
   useEffect(() => {
     async function fetchData() {
       const request = await tmdbApiClient.get(fetchUrl);
@@ -19,37 +15,19 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }
     fetchData();
   }, [fetchUrl]);
-  
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
 
-      autoplay: 1,
-    },
-  }
-  const handleClick = (movie) => {
-    if (trailerUrl) {
-      setTrailerUrl('');
-    } else {
-
-    }movieTrailer(movie?.name || "")
-    .then(url => {
-        const urlParams = new URLSearchParams(new URL(url).search);
-        setTrailerUrl(urlParams.get("v"));
-    }).catch(error => console.log(error));
-  }
-  
+  console.table(movies);
+  const s = "row_poster " + (isLargeRow ? "row_posterLarge" : "");
   return (
     <div className="row">
       <h2>{title}</h2>
 
       <div className="row_posters">
+        {/*several row_posters */}
+
         {movies.map((movie) => (
           <img
-          key= {movie.id}
-          onClick=  {() => handleClick(movie)}
-
+            key={movie.id}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -58,8 +36,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-          </div>
+    </div>
   );
 }
 
